312e14df9712503029935d0dc4aeee2d13dc0bfa
CelyaRousseau <celyarousseau.pro@gmail.com>
2015-11-18 13:14:15 +0100

    indentation spaces

--- neurone.py
+++ neurone.py
@@ -1,36 +1,36 @@
 class neurone(object):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
-		
+    """docstring for ClassName"""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+        
 
-	
+    
 
-	def hebb(f_net):
-	    return f_net
+    def hebb(f_net):
+        return f_net
 
-	def adjustWeight():
-		print("NEW WEIGHTS:")
-		return blop
+    def adjustWeight():
+        print("NEW WEIGHTS:")
+        return blop
 
-	def seuil():
-		# Function seuil
-		if n < 0:
-			return 1 
-		else :
-			return -1
+    def seuil():
+        # Function seuil
+        if n < 0:
+            return 1 
+        else :
+            return -1
 
 # Loi de hebb
 '''
-ai = -1	
+ai = -1 
 ai = 1
 
 aj = -1
-	ΔWij = R
-	ΔWij = -R
+    ΔWij = R
+    ΔWij = -R
 
-aj = 1	
-	ΔWij = -R
-	ΔWij = R
+aj = 1  
+    ΔWij = -R
+    ΔWij = R
 '''
